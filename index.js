18e30d973f0f358437819df0c449fb86645f1d17
Muhammad <extensionspc@gmail.com>
2024-10-13 16:41:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
 const ytdl = require('ytdl-core');
+const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors()); // Enable CORS
+app.use(cors());
 
 app.get('/api/video', async (req, res) => {
     const videoUrl = req.query.url;
@@ -12,7 +12,13 @@ app.get('/api/video', async (req, res) => {
     }
 
     try {
-        const info = await ytdl.getInfo(videoUrl);
+        const info = await ytdl.getInfo(videoUrl, {
+            requestOptions: {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+                },
+            },
+        });
         const downloads = info.formats.map(format => ({
             url: format.url,
             extension: format.container,
@@ -26,7 +32,7 @@ app.get('/api/video', async (req, res) => {
         });
     } catch (error) {
         console.error('Error fetching video:', error);
-        res.status(500).json({ error: 'Failed to fetch video data.' });
+        res.status(500).json({ error: `Failed to fetch video data: ${error.message}` });
     }
 });
 
